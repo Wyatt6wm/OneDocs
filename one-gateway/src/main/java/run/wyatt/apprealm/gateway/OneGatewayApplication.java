6ce962ae43e097dfc6b2f1c39b5065b60bf80228
Wyatt <wyatt6@163.com>
2023-04-16 09:56:34 +0800

    服务网关

--- one-gateway/src/main/java/run/wyatt/apprealm/gateway/OneGatewayApplication.java
+++ one-gateway/src/main/java/run/wyatt/apprealm/gateway/OneGatewayApplication.java
@@ -3,11 +3,13 @@ package run.wyatt.apprealm.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author Wyatt
  * @date 2023/4/15 15:01
  */
+@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class OneGatewayApplication {
